c86f770d923905623729a4b0dfe681afdfd2bbc5
shyamrai123 <raishyamkumar21@gmail.com>
2023-12-15 14:05:08 +0530

    final

--- src/components/viewJob.js
+++ src/components/viewJob.js
@@ -11,6 +11,7 @@ import Header from "./header";
 import { BsBookmark } from "react-icons/bs";
 import { verifyToken } from "../utils/utlis";
 import "../styles/viewJob.scss";
+import Footer from "./footer";
 
 function ViewJob() {
   const dispatch = useDispatch();
@@ -221,7 +222,7 @@ const userId = localStorage.getItem("userId")
           <div className="locationcomp container">
             <div>
             {
-              <div><b>{getJobDetails && getJobDetails.company_name}</b></div>
+              <div>{getJobDetails && getJobDetails.company_name}</div>
             }
             </div>
             <div>
@@ -238,45 +239,20 @@ const userId = localStorage.getItem("userId")
       </div>
 
       <div className="container verify ">
-         <button  className="btn-verify">Verify Account to Apply</button>
+         <button onClick={() => {
+          window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+         }}  className="btn-verify">Verify Account to Apply</button>
       </div>
 
          {/* ///footer// */}
 
      
 
-         <div className="foter container">
-               <div className="techie-div">
-                              <div>
-                                  <img className="techie-logo" src="https://res.cloudinary.com/cliqtick/image/upload/v1692600339/icons/logo-techie-_IE_uqk1bc.png"/>
-                              </div>
-                              <div className="sm-div">
-                                 <div><img className="sm-img" src="https://res.cloudinary.com/cliqtick/image/upload/v1686120164/techei_panda_website_images/Facebook-Icon_orvpxl.png" /></div>
-                                 <div><img className="sm-img" src="https://res.cloudinary.com/cliqtick/image/upload/v1686120165/techei_panda_website_images/LinkedIn-Icon_zcra9f.png" /></div>
-                                 <div> <img className="sm-img" src="https://res.cloudinary.com/cliqtick/image/upload/v1686120164/techei_panda_website_images/Instagram-Icon_ijchts.png" /></div>
-                              </div>
-                              <div className="" > https://techiepanda.in/</div>
-                              <div> +91 720 740 1718</div>
-               </div>
-               <div >
-                  <div className="resources" >Site Map</div>
-                  <div  className="resources">Resources</div>
-                  <div  className="resources">Blog</div>
-                  <div>FAQ</div>
-               </div>
-               <div className="techie-about">
-                   <div>Techie Panda</div>
-                   <div>About Us</div>
-                   <div>Meet Techie Panda</div>
-                   <div>Job Assistance</div>
-                   <div>Refund Policy</div>
-                   <div>Privacy and Cookie Policy</div>
-                   <div>Terms and Conditions</div>
-               </div>
-               <div className="techie-map">
-                   <div><iframe  className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3800.0386556256476!2d83.23167277586289!3d17.74281769246731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3967274842f4df%3A0x9711e68b73419d51!2sTechiepanda!5e0!3m2!1sen!2sin!4v1696832682466!5m2!1sen!2sin"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/></div>
-               </div>
-      </div>
+       <Footer/>
     </div>
   );
 }
